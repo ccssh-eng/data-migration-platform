8216d686ba1197f0c087360d3ce8b00d88b26d35
ccssh-eng <cirdec.labhub@gmail.com>
2026-04-07 16:36:32 +0200

    Mise à jour de pipeline et de fichier config_loader.py

--- scripts/config_loader.py
+++ scripts/config_loader.py
@@ -2,31 +2,25 @@ import os
 from urllib.parse import quote_plus
 
 
-class Config:
+def get_source_db_url():
+    password = quote_plus(os.getenv("SOURCE_DB_PASSWORD", ""))
 
-    @staticmethod
-    def get_source_db_url():
-        password = quote_plus(os.getenv("SOURCE_DB_PASSWORD"))
-
-        return (
-            f"{os.getenv('SOURCE_DB_TYPE')}+{os.getenv('SOURCE_DB_DRIVER')}://"
-            f"{os.getenv('SOURCE_DB_USER')}:{password}@"
-            f"{os.getenv('SOURCE_DB_HOST')}:{os.getenv('SOURCE_DB_PORT')}/"
-            f"{os.getenv('SOURCE_DB_NAME')}"
-        )
-
-    @staticmethod
-    def get_target_db_url():
-        password = quote_plus(os.getenv("TARGET_DB_PASSWORD"))
-        driver = quote_plus(os.getenv("TARGET_DB_ODBC_DRIVER"))
-
-        return (
-            f"{os.getenv('TARGET_DB_TYPE')}+{os.getenv('TARGET_DB_DRIVER')}://"
-            f"{os.getenv('TARGET_DB_USER')}:{password}@"
-            f"{os.getenv('TARGET_DB_HOST')}:{os.getenv('TARGET_DB_PORT')}/"
-            f"{os.getenv('TARGET_DB_NAME')}?"
-            f"driver={driver}"
-        )
+    return (
+        f"{os.getenv('SOURCE_DB_TYPE')}+{os.getenv('SOURCE_DB_DRIVER')}://"
+        f"{os.getenv('SOURCE_DB_USER')}:{password}@"
+        f"{os.getenv('SOURCE_DB_HOST')}:{os.getenv('SOURCE_DB_PORT')}/"
+        f"{os.getenv('SOURCE_DB_NAME')}"
+    )
 
 
-print("ENV TEST:", os.getenv("SOURCE_DB_URL"))
+def get_target_db_url():
+    password = quote_plus(os.getenv("TARGET_DB_PASSWORD", ""))
+    driver = quote_plus(os.getenv("TARGET_DB_ODBC_DRIVER", ""))
+
+    return (
+        f"{os.getenv('TARGET_DB_TYPE')}+{os.getenv('TARGET_DB_DRIVER')}://"
+        f"{os.getenv('TARGET_DB_USER')}:{password}@"
+        f"{os.getenv('TARGET_DB_HOST')}:{os.getenv('TARGET_DB_PORT')}/"
+        f"{os.getenv('TARGET_DB_NAME')}?"
+        f"driver={driver}"
+    )
