9e25323f9cfca3f84b9a7ca1df8f6ebb10af07bc
ccssh-eng <cirdec.labhub@egmail.com>
2026-05-21 15:03:12 +0200

    chore : reconstruire la structure du projet après une panne de disque dur local

--- terraform-azure/provider.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstatexxxx"
-    container_name       = "tfstate"
-    key                  = "data-migration-platform.tfstate"
-  }
-}
